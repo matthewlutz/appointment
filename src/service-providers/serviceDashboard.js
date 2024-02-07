a5970570672c600e50dfc141ed8de4a7b3d72c86
matthew lutz <matthewlutz06@gmail.com>
2024-02-07 10:59:46 -0600

    service provider dashboard-added day calendar

--- src/service-providers/serviceDashboard.js
+++ src/service-providers/serviceDashboard.js
@@ -3,6 +3,7 @@
 import React, {useState, useEffect} from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import './../index.css'; 
 
 function ServiceDashboard() {
@@ -40,40 +41,49 @@ function ServiceDashboard() {
             {/* Sidebar/dashboard */}
             <div className="w-1/5 min-h-screen bg-gray-800 p-5 border-r border-gray-200">
                 <h2 className="text-2xl font-semibold text-gray-100 mb-5">Dashboard</h2>
-                <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-blue-700 hover:bg-blue-600 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                     Home
                 </button>
                 <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                     Appointments
                 </button>
                 <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-purple-500 hover:bg-purple-600 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                    Services
+                    Edit Services
                 </button>
+                <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-red-500 hover:bg-red-600 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                    Appointment History
+                </button>
+                <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-blue-400 hover:bg-blue-400 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                    Analytics/Trends
+                </button>
+
+
                 {/* Add more buttons as needed */}
+                <button className="text-left w-full text-white font-semibold rounded py-2 px-4 mb-3 bg-gray-400 hover:bg-gray-400 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                    Settings
+                </button>
             </div>
+
 
             {/* Main content */}
             <div className="w-4/5 p-5">
                 <h2 className="text-2xl font-semibold mb-5">Welcome, {name}</h2>
                 <h2 className="text-xl font-semibold mb-5">Today's Appointments</h2>
+                <div className="w-full max-w-sm">
                 <FullCalendar
-                    plugins={[dayGridPlugin]}
-                    initialView="dayGridDay"
+                    plugins={[timeGridPlugin]}
+                    initialView="timeGridDay"
                     slotMinTime="09:00:00"
                     slotMaxTime="17:00:00"
                     height="auto"
                     slotDuration="00:30:00"
-                    headerToolbar={{
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'dayGridDay,dayGridWeek,dayGridMonth'
-                    }}
+                    nowIndicator={true}
                     allDaySlot={false}
                     expandRows={true}
                     stickyHeaderDates={true}
-                    themeSystem='bootstrap' 
-                    
+                    themeSystem='bootstrap'                     
                 />
+                </div>
             </div>
         </div>
     );

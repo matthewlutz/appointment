5a0e89a8dd4510a5dde146190410c4f49d34e3a9
max <maxvagle@comcast.net>
2024-02-23 14:40:54 -0600

    fixes errors with implementing the css page

--- src/admin/UsersList.js
+++ src/admin/UsersList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './styles/UsersListPage.css';
+import '../common/styles/UsersListPage.css';
 //import {Link, useParams, useLocation, useNavigate} from 'react-router-dom';
 
 function UsersList(){
